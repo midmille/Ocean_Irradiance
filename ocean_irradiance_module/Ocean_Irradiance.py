Finished calculation of the fixed point method over the ROMS domain for 3,10,20 shots all on a log grid

ocean_irradiance_module/Ocean_Irradiance.py
@@ -519,9 +519,9 @@ def ocean_irradiance(hbot, Ed0, Es0, Euh, ab_wat, coefficients, phy = None, N = 
     elif pt1_perc_zbot == False: 
         zbot = hbot 
     ## log transformed z grid.
-    # z = Log_Trans(zbot, N) 
+    z = Log_Trans(zbot, N) 
     ## linear z 
-    z = np.linspace(zbot, 0, N)
+    #z = np.linspace(zbot, 0, N)
     
     
     
@@ -707,9 +707,9 @@ def ocean_irradiance_shoot_up(hbot, Ed0, Es0, Euh, ab_wat, coefficients, phy = N
     elif pt1_perc_zbot == False: 
         zbot = hbot 
     ## log transformed z grid.
-    # z = Log_Trans(zbot, N) 
+    z = Log_Trans(zbot, N) 
     ## linear z 
-    z = np.linspace(zbot, 0, N)
+    #z = np.linspace(zbot, 0, N)
     
     
     
@@ -776,7 +776,7 @@ def ocean_irradiance_shoot_up(hbot, Ed0, Es0, Euh, ab_wat, coefficients, phy = N
     return Ed, Es, Eu, z
 
 
-def ocean_irradiance_shoot_fp(hbot, fp, fpi, Ed0, Es0, Euh, ab_wat, coefficients, phy = None, N = 30, 
+def ocean_irradiance_shoot_fp(hbot, Ed0, Es0, Euh, ab_wat, coefficients, phy = None, N = 30, 
                      pt1_perc_zbot = True):
     
     
@@ -862,7 +862,7 @@ def ocean_irradiance_shoot_fp(hbot, fp, fpi, Ed0, Es0, Euh, ab_wat, coefficients
     Eu1[0] = Euh ##bottom
     
     ## Default number of shots for BVP shoot method solution
-    shots = 10
+    shots = 20
     
     ##unpacking the ab_wat_tuple 
     a_wat,b_wat = ab_wat 