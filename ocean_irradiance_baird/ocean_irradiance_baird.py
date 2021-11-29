1ce14f4c8c807377d871334743ad381b95a3cecc
Miles Miller <midmille@ucsc.edu>
2021-11-29 08:30:16 -0800

    Started the baird ocean irradiance

---
+++ ocean_irradiance_baird/ocean_irradiance_baird.py
@@ -0,0 +1,33 @@
+"""
+Created: 29 November 2021
+Author: Miles D Miller 
+
+The implementation of the Baird et al. method for calculating the 
+remote sensed reflectance given absorbtion and scattering profiles. 
+
+The reference article is: 
+
+Mark E. Baird 
+27 November 2015
+Remote-sensing reflectance and true colour produced by a coupled 
+hydrodynamic, optical, sediment, biogeochemical model of the 
+Great Barrier Reef, Australia: Comparison with satellite data
+
+"""
+
+## External
+import numpy as np
+
+## User
+import ocean_irradiance_module.Ocean_Irradiance as OI
+
+def ocean_irradiance_baird():
+
+    """
+    
+    """
+
+    
+    
+
+
