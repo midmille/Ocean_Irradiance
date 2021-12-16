Found bug with calculation of zbot in function 'zbot_func' in Ocean_Irradiance.py. The zbot array was not flipped in the necessary loop. See notes from 12/16. The bug has been corrected and now it is necessary to redo some studies

ocean_irradiance_module/Ocean_Irradiance.py
@@ -35,7 +35,7 @@ class Phy:
         elif phy.ndim == 2:
             self.Nphy = np.shape(phy)[1]
             assert self.Nphy == len(a) and self.Nphy == len(b)
-            assert self.NPhy == len(esd)
+            assert self.Nphy == len(esd)
 
         assert np.shape(phy)[0] == self.Nz
         
@@ -201,7 +201,7 @@ def zbot_func(Ed0, c, light_frac = .01, phy=False, z=None):
     for k, Ed_i in enumerate(np.flip(Ed)) :
         EdoEd0 = Ed_i / Ed0
         if EdoEd0 < light_frac :
-            zbot = zbots[k] 
+            zbot = np.flip(zbots)[k] 
             return zbot
    
         
@@ -718,7 +718,7 @@ def ocean_irradiance_shoot_up(hbot, Ed0, Es0, Euh, ab_wat, coefficients, phy = N
     Nm1 = N - 1  
 
     ##initial guess... doesn't matter too much
-    init_guess = .001
+    init_guess = .2
     
     Ed1 = np.full(N, init_guess)
     Es1 = np.full(N, init_guess)

ocean_irradiance_module/Ocean_Irradiance_ROMS.py
@@ -226,8 +226,8 @@ def Ocean_Irradiance_Field(mask, ab_wat, ab_diat, ab_syn, chl_diatom, chl_nanoph
                 a = np.array([ab_diat[0], ab_syn[0]])
                 b = np.array([ab_diat[1], ab_syn[1]])
                 
-                phy = Ocean_Irradiance.Phy(z_r0, phy_profs, a, b)    
-                
+                phy = Ocean_Irradiance.Phy(z_r0, phy_profs, [ESD('Diat'), ESD('Syn')], a, b)    
+
                 if method == 'shoot_down':
                     
                     ocean_irr_sol = Ocean_Irradiance.ocean_irradiance(hbot,E_d_0,E_s_0,E_u_h,

ocean_irradiance_studies/Artificial_Profile_Sampling/artificial_prof_samp.py
@@ -40,7 +40,7 @@ def Sampling_Sensitivity(PI, zbot, N_irr, phy_type, wavelengths):
         for k, N_samp in enumerate(N_samps):
             ## Creating the sample grid.
             z_samp = np.linspace(zbot, 0, N_samp)
-            phy_samp = OI.artificial_phy_prof(z_samp, 0, 100, .001)
+            phy_samp = OI.artificial_phy_prof(z_samp, 0, 40, 1)
             phy_samp[phy_samp < 1e-6] = 0
 
             ## Phytoplankton object. 
@@ -102,7 +102,7 @@ def Plot_Sampling_Sensitivity(N_samps, chla_bias):
 if __name__ == '__main__': 
 
     PI = Param_Init()
-    zbot = -500
+    zbot = -100
     N_irr = 200
     phy_type = 'Diat'
     wavelengths = [443, 551]

ocean_irradiance_studies/Irradiance_Method_Comparison/plot_method_comparisons.py
@@ -25,7 +25,7 @@ def Load_Fields(irr_out_dir):
     
     irr_out_scipy = pickle.load(open(f'{irr_out_dir}/irradiance_out_scipy.p', 'rb')) 
     irr_out_shoot_down = pickle.load(open(f'{irr_out_dir}/irradiance_out_shoot_down_log.p', 'rb')) 
-    irr_out_shoot_up = pickle.load(open(f'{irr_out_dir}/irradiance_out_shoot_up_log.p', 'rb')) 
+    irr_out_shoot_up = pickle.load(open(f'{irr_out_dir}/irr_field_shoot_up.p', 'rb')) 
     irr_out_shoot_fp = pickle.load(open(f'{irr_out_dir}/irradiance_out_shoot_fp_20_shots.p', 'rb')) 
     irr_out_dut = pickle.load(open(f'{irr_out_dir}/irradiance_out_dut.p', 'rb')) 
 
@@ -255,7 +255,11 @@ def main():
     ## The ROMS output oobject 
     R_nc = RRO.ROMS_netcdf(ROMS_file)
 
-    #Plot_Irradiance_Fields( Ei, fields, methods, wavelengths, R_nc, plot_surface=True, plot_min=True, plot_max=True, plot_surf_diff = True) 
+    Plot_Irradiance_Fields( Ei, fields, methods, wavelengths, R_nc, plot_surface=True, plot_min=True, plot_max=True, plot_surf_diff = True) 
      
     Plot_Number_Bad_Profiles(Ei, fields, methods, wavelengths, R_nc)
    
+
+if __name__ == '__main__':
+
+    main()